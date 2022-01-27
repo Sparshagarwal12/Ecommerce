729dfb8df27a5724c3539fe3dc3b204f7380f846
Sparshagarwal12 <mangalsanchit20@gmail.com>
2022-01-27 13:01:56 +0530

    cart page done

--- src/screens/cart/Cart.jsx
+++ src/screens/cart/Cart.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Calculation from '../../component/calculation/Calculation';
 import '../cart/cart.css';
-import { topProduct } from '../../model/product-model';
+import CartCard from '../../component/CartCard';
 
 function Cart() {
     return <div className='cart-page-view'>
-        <div className='cart-page-card'>
-            <div className='cart-item-column'>
-                <img src={topProduct[0].image} className='cart-page-product-image'/>
-            </div>
-        </div>
+        <CartCard />
         <Calculation />
     </div>;
 }
